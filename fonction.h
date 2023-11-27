25e6cd7c25a00e615f63bb6bc11797074ab41d26
ImIdir <idiraitdjoudi@hotmail.fr>
2023-11-27 09:31:55 +0100

    premiere question

--- fonction.h
+++ fonction.h
@@ -1,12 +1,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/wait.h>
 #include <stdio.h>  
 #include <stdlib.h> 
-#include <time.h>
 
 #define WELCOME  "Bienvenue dans le Shell ENSEA. \nPour quitter, tapez 'exit'. \n"
 
